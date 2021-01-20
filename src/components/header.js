f12234352edcba6cb805664501dcd2a25ac7d6ec
Haemin Ryu <13105504+happygoals@users.noreply.github.com>
2021-01-20 12:04:08 -0500

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -167,7 +167,7 @@ const Header = (props) => {
   return (
     <Div article={props.article}>
       <LogoWrapper article={props.article}>
-        <img src="../../img/android-chrome-192x192.png">
+        <img src="../../../static/android-chrome-192x192.png">
         {props.article && (
           <NameLink href="/">
             <NameArticle article={props.article}>Haemin Ryu</NameArticle>
