3986ba4cfb015efd9c2c0339f76bf48cfc7243a4
Haemin Ryu <13105504+happygoals@users.noreply.github.com>
2021-01-22 15:17:41 -0500

    Update bamlogo.js

--- src/img/bamlogo.js
+++ src/img/bamlogo.js
@@ -3,7 +3,7 @@ import React from 'react'
 const BamLogo = () => {
   return (
 <svg version="1.0" xmlns="http://www.w3.org/2000/svg"
- width="120.000000pt" height="120.000000pt" viewBox="0 0 500.000000 500.000000"
+ width="110.000000pt" height="110.000000pt" viewBox="0 0 500.000000 500.000000"
  preserveAspectRatio="xMidYMid meet">
 
 <g transform="translate(100.000000,400.000000) scale(0.100000,-0.100000)"
