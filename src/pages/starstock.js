ae4907578fc5132ba15ca794f2845be05bf739a4
happygoals95@gmail.com <happygoals95@gmail.com>
2021-01-27 01:21:08 -0500

    first finalization of starstock project descriptions

--- src/pages/starstock.js
+++ src/pages/starstock.js
@@ -16,6 +16,9 @@ import StarstockImage1 from 'img/articles/starstock/starstock1.jpg'
 import StarstockImage2 from 'img/articles/starstock/starstock2.jpg'
 import StarstockImage3 from 'img/articles/starstock/starstock3.jpg'
 import StarstockImage4 from 'img/articles/starstock/starstock4.jpg'
+import StarstockImage5 from 'img/articles/starstock/starstock5.png'
+import StarstockImage6 from 'img/articles/starstock/starstock6.png'
+import StarstockImage7 from 'img/articles/starstock/starstock7.png'
 
 const Section = styled.section`
   margin: 64px 0;
@@ -34,182 +37,122 @@ const Starstock = () => {
           <h2>Build a “galactic stock market” app</h2>
           <p>
             StarStock App is an application I designed in
-            order to learn human computer interaction design and solve specific problems from the commercial products for stock markets.<br />
+            order to solve specific problems from the commercial products for stock markets with imagination of galactic stock market which manages the planet transaction.<br />
           </p>
           <br />
           <h3>Why galactic stock market app?</h3>
           <p>
-            Most existing apps focus on the financial stock market. In the future, there will be high chance to be needed an app  for buying, selling, and managing planets as stocks in the space world.
+            Nowadays, most existing apps focus on the financial stock market. Looking ahead to the more distant future, there will be high chance to be needed an app for buying, selling, and managing planets as stocks in the space world.
           </p>
           <p>
-            The resulting color schemes might be suitable for logos or classic
-            graphic design, but they rarely work for UI Design, where most of
-            the time you need to work with a few base colors (that are often set
-            by brand guidelines) and a whole range of different variations and
-            shades.
+            It is not just in my imagination, but actually there are video games that involve the galaxy market and galaxy trade in the real world, and the movie Star Wars showed the galactic economy part of the story.
           </p>
           <p>
-            What Steve doesn't describe exactly is how he finds those colors:
-            <i>
-              "...you just need to fill in the gaps in between", "Trust your
-              eyes, not the numbers."
-            </i>
+            The resulting application might be suitable for easier trade of stars with search for star stocks by categorization, related news article curation service, and star stock recommendation service based on the user's preferences.
           </p>
+
+          <h3>User stories and Usability</h3>
 
           <p>This is where StarStock app comes into play:</p>
           <ul>
             <li>
-              Build <b>smooth color schemes</b> that flow from one color to
-              another
+             <b>Search</b> for star stocks that you want.
             </li>
             <li>
-              <b>Finetune hue, saturation value gradients</b> of your palette
-              and of each color swatch individually
+              Show the <b>market flow</b> and <b>market prices</b> for buyers and sellers in a list.
             </li>
             <li>
-              Import, analyze and <b>edit existing color schemes</b>, find
-              errors or automatically find the most similar colors from a new
-              palette to a list of legacy colors from your codebase
+              <b>Buy star stocks</b> with comparison for multiple planets and get the stocks that fits your <b>circumtances</b> and <b>preferences.</b>
+            </li>
+            <li>
+              <b>Sell star stocks</b> and get the money back to my account.
+            </li>
+            <li>
+              Check the <b>profit</b> and <b>loss ratio</b> like portfolios.
             </li>
           </ul>
           <br />
-          <h3>Building a new color palette</h3>
-          <p>
-            To create a new color palette, click on the plus icon in the left
-            sidebar and delete all sample colors by clicking on backspace. You
-            can now add your base color by clicking "add swatch" or hitting the
-            spacebar and entering the HEX-Value in the right sidebar.
-          </p>
-  
-            <h3>User stories and Usability/design requirements</h3>
-              <p> The usability requirements should consider design elements we have discussed in class, such as keeping design consistent, using signifiers, etc. </p>
-              <p>As a buyer and seller, I would like to search for stocks so that I can see the market flow and stock prices.</p>
-              <p> As a buyer, I would like to buy stocks such that I can get the stocks between multiple planets.</p>
-              <p> As a seller, I would like to sell stocks such that I can get the money back to my account.</p>
-              <p> As a buyer and a seller, I would like to check the profit and loss ratio.</p>
-  
-          <h4>Usability / design requirements</h4>
-             <p>Keep the design concept of the app like space and planets with dark colors.</p>
-             <p>The signifiers should be simple and easy to understand.</p>
-             <p>Consider security part since the application is related to the payment process.</p>
-  
+          <h3>Usability / Design requirements</h3>
+          <p> The usability requirements should consider design elements, such as keeping design consistent, using signifiers, etc. </p>
+          <p>This is what I consider for StarStock app:</p>
+             <li>Keep the design concept of the app like space and planets with dark colors.</li>
+             <li>The signifiers should be simple and easy to understand.</li>
+             <li>Buttons include the text.</li> 
+             <li>Consider security part since the application is related to the payment process.</li>
+          <br />
           <h3>Task Analysis</h3>
-  
-  
-          <h3> Design Alternative for the UI of the app </h3> 
-            <p> I clearly label the affordances and the signifiers you use for those affordances. </p> 
-  
+          <p> This is an example of task analysis on one of the user stories. </p>
+          <LargeImage src={StarstockImage5} />
+          
+          <br />
+          <br />
+          <h3> Design Alternative for the User Interface of the app </h3> 
+           
           <LargeImage src={StarstockImage0} />
           <p>
-            When adding more swatches you can see that three curves form: one
-            for the hue, one for the saturation and one for the value
-            (=brightness) of the colors.
+          A basic system color for UI Design consists of 7 colors. 
           </p>
+          <p>
+           According to the design requirements, I keep a cosmic image using the dark blue / deep dark blue color as a main background color. Since the button or any content like icons should be noticeable and clickable, I chose their color as yellow (Yellow is a complementary color of dark blue or purple.) the most. For the badges and buttons that uses for significant decision such as buying/selling stocks and saving operation, I used 
+           fluorescent colors. 
+          </p>
+
+          <p> Here is the affordances and the signifiers I used for those affordances.</p>
           <LargeImage src={StarstockImage1} />
-          <p>
-            A basic palette for UI Design consists of 8-10 shades where the hue
-            stays the same for all shades, the value goes up and the saturation
-            goes down <b>smoothly</b> as the colors get lighter.
-          </p>
+
           <br />
-          <h3>Analyzing existing stock market apps</h3>
+          {/* <h3>Analyzing existing stock market apps</h3> */}
+          
+          <h3>Prototyping</h3>
           <p>
-            If you already have a color palette and you want to know if it can
-            be optimized, import the colors one by one or write the palette down
-            in the special JSON format (example can be seen whan clicking on
-            import) and batch import your palette.
-          </p>
-          <p>
-            Here is the palette Steve Schoger uses in his example in Refactoring
-            UI:
+            Here are the example screens of StarStock app: 
           </p>
   
  
   
           <LargeImage src={StarstockImage2} />
-         <p> I produced a HIGH-FIDELITY PROTOTYPE of the application using Adobe XD. </p>
-            <p>
-            As you can see there is a little ditch in the value envelope curve
-            and the saturation curve is round in the beginning and falls off in
-            a linear way suddenly, nothing too serious but it's clear that Steve
-            has chosen his colors by "trusting his eyes" and not his numbers ;).
+          <p> I produced a high fidelity prototype of the application using Adobe XD. 
+            As you can see, there 4 different screens in the process of searching for stocks. 
+          </p>
+          <h5>Searching interested star stocks</h5>
+          <p>
+            In the first screen, you can view the news by categorizing the stocks with keywords. When you search for stocks like Mars, the result of your search displays your interest planet. The result contains a horizontal bar graph to show the percentage of positive and negative news depends on the artificial analysis curating system. When you click the related keywords in the pill shape buttons, it brings the daily news that are associated with the planet you are interested in and the keywords you selected. The reason why the daily news are shown as two column is because that can show the news titles as summary as many as possible.             
           </p>
           <p>
-            You can edit lines by dragging the handles around or by selecting
-            two handles and clicking on "interpolate" (only linear interpolation
-            is supported at the moment).
+            When you scroll down the second screen above will appear with keywords you chose as bigger yellow texts on the top. In the center of the screen, you can see the stars around Mars (the one you searched) with the name, red color, and sphere shape. If you press one star, it turns as bluish color star and presents related news for that star. 
           </p>
+          <h5>Finding your fits</h5>
+          <p>Moreover, in order for the first-time users or beginners who are not familiar with star stocks, the application has user settings for the stock recommendations. If you inputs your total budget for star stocks and the range of risks you can afford, you can know what your volatility type for stocks is. When you are happy with your result, hit SAVE and your type will be saved in your settings.</p>
+
           <LargeImage src={StarstockImage3} />
+          <h5>Checking the list of star stocks</h5>
+
           <p>
-            When you are happy with your result, simply export your new colors
-            again.
+            With starstock app, you can simply view the list of the stars including the image, name, chip to describe a stock of a particular quality, and the amount currently being traded. 
+            The color of the price text is meaningful based on the common color scheme use in the stock market. Green indicates the stock is trading higher than the previous day's close. Red indicates the stock is trading lower than the previous day's close. Blue or white means the stock is unchanged from the previous closing price. 
+          </p>
+          <p>
+            When you swipe from right to left on the item, the buttons for redirecting to the buying or selling options will appear. 
           </p>
           <br />
-          <h3>Mapping old colors to new ones</h3>
-          <p>
-            You will often somehow find yourself in a situation where you have
-            built a wonderful new color palette, but your legacy code does not
-            reflect the beauty of it as a lot of different shades and colors
-            have piled up over time.
-          </p>
-          <p>
-            With starstock app, you can import those (legacy) colors and find the
-            most similar colors from your new palettes automatically. You will
-            also immediatly spot when colors are too far off, and you need to
-            introduce more colors in your palettes.
-          </p>
+          
           <LargeImage src={StarstockImage4} />
           <p>
-            It can also be useful if you want to add shades to your palette and
-            need to rename all existing colors.
+            The application can also be useful if you want to get information of the galaxy. After you chose the galaxy you are interested in, the actual photo of the star displays with other stars that are located around the star. If you swipe the card at the bottom of the screen that includes the detail of the star, then you can view the star that is next to the star you viewed. When you go to 'Show Charts', it exhibits the stock trading chart (candle chart). You can check other scopes like profitability, growth, stability, and projection in the next page.  
           </p>
-  
+          <LargeImage src={StarstockImage6} />
+          <p>
+            There are more features for the starstock app.
+          </p>
+          <br />
           <h3> User Testing for the prototype</h3> 
- <p> Usability test consent form </p>
-
-  <p>
-This is a study about galactic stock market application, a tool buying, selling, and managing stocks between
-multiple planets stock markets in a generic Sci-Fi Galactic Federation. The goal of this project is to make
-the mobile application for galactic stock market appealing, intuitive and user friendly. Your participation
-will help us achieve this goal.
-Please read this form and sign it.
-In this usability test:
-● You will be asked to perform certain tasks on a computer.
-● We will also offer you an interview on the tasks you have performed.
-Participation is voluntary in this survey of usability. Both details will be kept strictly secret. To help
-improve the Kinect StarStock application, the descriptions and findings can be used. However, you can
-withdraw your testing consent at any time and reschedule participation.
-Please contact Haemin Ryu at ryuh01@pfw.edu if you have any questions after today, I read and
-understood the details on this form and answered all my questions.
-______________________________ _________________
-Signature Date
-
-List of Tasks
-
-1. Open the StarStock application
-2. Learn by tutorial and click “Skip” button
-3. Create your account and sign in using your new account
-4. Press the pin numbers to make sure authorization can be worked
-5. Try to customize the application setting on Settings
-6. Try to feel all the screens by swiping the screens
-User Feedback Survey
-
-1. How convenient was the steps of the application? Was it easy and clear to follow the instruction or
-signifier?
-2. Were you able to access the settings page?
-3. How do you feel about the selecting date and time on galaxy stock trading chart?
-4. Did the buttons and scroll screens work efficiently and smoothly?
-5. Is the user interface simple? Or complex?
-6. How do you think of the processes(steps) of the app usage experience? Is it understandable and
-intuitive?
-7. Are the locations of the navigation bar, buttons, texts, contents proper?
-  
-  </p>
+           <p> Here is an example of the usability test consent form: </p>
+           <LargeImage src={StarstockImage7} />
           <br />
           <h3>Feedback welcome</h3>
           <p>
-            If you have ideas how to make this tool better, please let me know
-            via <a href="https://twitter.com/haeminryu_">Twitter</a>.
+            If you have ideas to make this application better, please let me know
+            via <a href="https://twitter.com/haeminryu_">Twitter</a>. Thank you for reading! 
           </p>
         </ContentWrapper>
       </Section>
