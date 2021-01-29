b85fa00886f88ee91067c3ac07c2fdc8acfc031c
Haemin Ryu <13105504+happygoals@users.noreply.github.com>
2021-01-29 16:38:53 -0500

    Update work.js

--- src/components/work.js
+++ src/components/work.js
@@ -13,6 +13,7 @@ import BamLogo from 'img/bamlogo'
 const EbridgeLink = <Link to="/ebridge">Read More</Link>
 const StarstockLink = <Link to="/starstock">Read More</Link>
 const BMWLink = <a href="https://vimeo.com/392362212">Watch Demo on Vimeo ↗</a>
+const BMWLink2 = <a href="https://drive.google.com/file/d/1BQDRDp9zB7wnmT2Y8-V1Cqo7ibkfhxBc/view?usp=sharing">Read More</a>
 const ParkviewLink = <a href="https://github.com/happygoals/Team7_PFW_Capstone">Read More on GitHub ↗</a>
 
 const Work = () => {
@@ -38,7 +39,7 @@ const Work = () => {
             logo={BMWLogo()}
             title="BMW InPA 5.0.6 Tool"
             abstract="This is a redesign project for BMW InPA 5.0.6 Tool to allow everyday BMW InPA tool users to have more control over how their car functions."
-            link={BMWLink}
+            link={BMWLink, BMWLink2}
           />
           <Project
             logo={BamLogo()}
