26a9584cbb255132d880f2ffd17702f031e3dc29
Haemin Ryu <13105504+happygoals@users.noreply.github.com>
2021-02-03 16:18:22 -0500

    Update parkview.js

--- src/pages/parkview.js
+++ src/pages/parkview.js
@@ -28,7 +28,7 @@ const Bam = () => {
   return (
     <GlobalWrapper>
       <Header title="Indoor Navigation: Improved Accessibility for People with Disabilities" />
-      <CoverImage src={ParkviewCoverImage} focusX={'44%'} focusY={'65%'} />
+      <CoverImage src={ParkviewCoverImage} focusX={'44%'} focusY={'75%'} />
       <Section>
         <ContentWrapper>
           <h2>Products with extra bäm effect</h2>
