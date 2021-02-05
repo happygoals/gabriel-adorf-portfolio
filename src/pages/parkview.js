3c0e41cc1cf91de10d1b3a811ebb6f93e4a8461c
Haemin Ryu <13105504+happygoals@users.noreply.github.com>
2021-02-05 12:30:30 -0500

    Update parkview.js

--- src/pages/parkview.js
+++ src/pages/parkview.js
@@ -49,14 +49,13 @@ const Pakview = () => {
         </ContentWrapper>
         <ContentWrapper>
           <h2>Project Scope Description</h2>
-          <p>
-           Problem : Information in the database is not currently used.
-          </p>
-          <p>
-           Reason : Hospital patient’s location and database data can be efficiently managed to help guide patients. * Example Data: Age, Time, Handicapped
-          </p>
+          <p>Problem</p>
+          <p>Information in the database is not currently used.</p>
+          <p>Reason</p>        
+          <p>Hospital patient’s location and database data can be efficiently managed to help guide patients.</p>
+          <p> * Example Data: Age, Time, Handicapped</p>
          <p>
-           Solution Development of a Web Application that presents analyzed data to administrative staff in a comprehensive manner and allowing the data to be manipulated or viewed in many different formats and the Web application is only for staff members.
+           <p>Solution Development of a Web Application</p> that presents analyzed data to administrative staff in a comprehensive manner and allowing the data to be manipulated or viewed in many different formats and the Web application is only for staff members.
          </p>
           <p>
            For example, using the webpage the Parkview staff could determine what time are most populated, and by which age group or gender. This would allow for an in-depth look at particular patient demographics, giving Parkview the tools to improve upon their already great services.
