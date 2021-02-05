2fca8db1c9f6267a24009792e1ed3072d1eff5ab
Haemin Ryu <13105504+happygoals@users.noreply.github.com>
2021-02-05 12:32:19 -0500

    Update work.js

--- src/components/work.js
+++ src/components/work.js
@@ -12,11 +12,10 @@ import ParkviewLogo from '../img/parkviewlogo'
 
 const EbridgeLink = <Link to="/ebridge">Read More</Link>
 const StarstockLink = <Link to="/starstock">Read More</Link>
-const BMWLink = <a href="https://vimeo.com/392362212">Watch Demo on Vimeo ↗</a>
-const BMWLink2 = <a href="https://drive.google.com/file/d/1BQDRDp9zB7wnmT2Y8-V1Cqo7ibkfhxBc/view?usp=sharing">Read More</a>
-const ParkviewLink = <a href="https://vimeo.com/392359571/6adb6cafd8">Watch Demo on Vimeo ↗</a>
-const ParkviewLink2 = <Link to="/parkview">Read More</Link>
-
+const BMWLink = <a href="https://drive.google.com/file/d/1BQDRDp9zB7wnmT2Y8-V1Cqo7ibkfhxBc/view?usp=sharing">Read More</a>
+const BMWLink2 = <a href="https://vimeo.com/392362212">Watch Demo on Vimeo ↗</a>
+const ParkviewLink = <Link to="/parkview">Read More</Link>
+const ParkviewLink2 = <a href="https://vimeo.com/392359571/6adb6cafd8">Watch Demo on Vimeo ↗</a>
 const Work = () => {
   return (
     <TwoColumns
@@ -40,15 +39,15 @@ const Work = () => {
             logo={BMWLogo()}
             title="BMW InPA 5.0.6 Tool"
             abstract="This is a redesign project for BMW InPA 5.0.6 Tool to allow everyday BMW InPA tool users to have more control over how their car functions."
-            link={BMWLink}
-            link2={BMWLink2}
+            link={BMWLink2}
+            link2={BMWLink1}
           />
           <Project
             logo={ParkviewLogo()}
             title="Indoor Navigation: Improved Accessibility for People with Disabilities"
             abstract="This project is a web application that analyzes and presents created data in a heatmap and a chart of frequent routes. Parkview hospital could understand routing patterns with the framework and optimize space utilization for people with disabilities."
-            link={ParkviewLink}
-            link2={ParkviewLink2}
+            link={ParkviewLink2}
+            link2={ParkviewLink1}
           />
         </Fragment>
       }
