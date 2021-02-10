b22a4a48618fbcf3125e1f9af665c12a19374935
Haemin Ryu <13105504+happygoals@users.noreply.github.com>
2021-02-10 13:27:54 -0500

    Update ebridge.js

--- src/pages/ebridge.js
+++ src/pages/ebridge.js
@@ -11,7 +11,7 @@ import Footer from 'components/footer'
 import FullSizeImage from 'components/article/fullsizeimage'
 
 import CoverSrc from 'img/articles/ebridge/lifetime-cover.svg'
-import SilosSrc from 'img/articles/ebridge/lifetime-silos.svg'
+import DiagramSrc from 'img/articles/ebridge/ebridge-diagram.png'
 import Silos2Src from 'img/articles/ebridge/lifetime-silos2.svg'
 import UISrc from 'img/articles/ebridge/lifetime-app.jpg'
 import UI2Src from 'img/articles/ebridge/lifetime-app2.jpg'
@@ -106,15 +106,11 @@ const Ebridge = () => {
       <CoverImage src={CoverSrc} focusX={'35%'} focusY={'50%'} />
       <Section>
         <ContentWrapper>
-          <h2>Data flow in medicine is broken</h2>
-          <h3>We have no easy access to our own medical data</h3>
+          <h2>Covid 19 has caused the disruption in face-to-face interaction between recruiters and candidates.</h2>
+          <h3>Shallow networking and resume spreading does not help with recruiting.</h3>
           <p>
-            Medical practices are some of the last workplaces in Germany where a
-            lot of communication with clients (patients) and colleagues (other
-            doctors) is still done paper-based or verbally. Important
-            information, is written down illegibly by hand, is only briefly
-            mentioned - or in the worst case - not handed out to patients at
-            all.
+            Students want to interact with professionals who understand the position.
+
           </p>
           <p>
             Historically, the fact that people are not granted direct access to
@@ -139,7 +135,7 @@ const Ebridge = () => {
             control to outsiders so readily and &quot;hope for the best&quot;
             without trying to understand the basis on which decisions are made.
           </p>
-          <h3>Doctors need access to the medical history of patients</h3>
+          <h3>Recruiters need a tool to manage a number of resumes</h3>
           <p>
             People - especially chronically ill - who actually try to keep track
             of past treatments and diagnoses usually do so in form of a folder
@@ -174,15 +170,12 @@ const Ebridge = () => {
             uses anymore.
           </p>
         </ContentWrapper>
-        <FullSizeImage src={SilosSrc} />
+        <FullSizeImage src={DiagramSrc} />
         <ContentWrapper>
-          <h3>Computers can not help to take medical decisions.</h3>
+          <h3>Our Activity Diagram</h3>
           <p>
-            If previous findings are not digital, structured and do not contain
-            any meta-information, the data is not very useful to computers.
-            There are efforts to make medical documents understandable for
-            machines through OCR software. However, the road to meaningful
-            results on this path is still long.
+           Each view starts with candidates create profiles and Professionals create their accounts. 
+           The steps candidates would do from their view (Candidate View) and the steps recruiters would do from their view (Professional View). 
           </p>
           <p>
             For your diagnoses, doctors therefore rely to a large extent on the
@@ -208,194 +201,97 @@ const Ebridge = () => {
         <FullSizeImage src={Silos2Src} />
         <ContentWrapper>
           <h2>Our approach</h2>
-          <h3>Secure infrastructure for medical data</h3>
+          <h3>Resume Gallery & Video Gallery</h3>
           <p>
-            The path to computer-assisted medical treatment begins with
-            structured collection and aggregation of the patient&#39;s medical
-            context. This data is then supplemented by new information that is
-            created during an appointment or an hospital stay and ends with all
-            newly acquired information and treatment instructions being
-            structured and fully returned into the hands of the patient.
+            Candidates must upload their resume on the system as they create their profile.
+            After finishing the profile, our system will have predefined suggested questions for them to address in a short 3 minutes video. They must complete this step for the profile to be visible to recruiters.
+            After they completed these steps, besides demonstrated in their home page, the resume and video will be added to the gallery, which is organized by majors. 
+
           </p>
+          <h3>Resume Gallery</h3>
           <p>
-            Patients should be considered intelligent enough to have a say. They
-            should be allowed to ask questions and to question decisions.
-            Interfaces are needed to give them insights into their treatment
-            processes and enable them to browse through their medical history
-            and draw conclusions from it.
+            Visible to recruiters by school / majors / work authorization, etc.
+            Can be used as a resume cloud for the recruiter.
+            The system suggests resume to include the predefined keywords once candidate upload their resume. Recruiters can search by skills and find the resume containing the skills they need.
+            Wish-list favorite resume increase points for candidate.
+            Algorithm to suggest potential resumes to recruiters based on the current openings. 
           </p>
+          <h3>Video Gallery</h3>
           <p>
-            Doctors should be supported in their difficult work by the best
-            possible data basis and clear and just as helpful interfaces, which
-            aggregate this data base in a meaningful and understandable way and
-            thus enable raw computing power to be used for medical purposes.
+            Visible to recruiters by majors (Accounting, for example).
+            The video is hoped to eliminate the need for phone screen interview and help recruiter select those that will move straight to the interview with people in the position.
+            Wish-list option for their favorite video and refer once they have an opening position.
+            Algorithm to suggest videos to recruiters based on the current openings. 
           </p>
+          
         </ContentWrapper>
         <FullSizeImage src={UISrc} />
         <ContentWrapper>
-          <h3>Decentral & mobile first</h3>
+          <h3>Earning Badges</h3>
           <p>
-            In the past several companies have tried to solve the problems with
-            centralized cloud platforms where both doctors and patients can log
-            in.
+            Current job-seeking platforms allow users to add skills to their profiles. 
+            However, it is a fact that it is difficult to know if those listed skills are VALID.
           </p>
           <p>
-            The difficulty always lies in building a link between the two sides
-            and a way for the encryption keys to be exchanged and stored. In
-            some systems, the patients have to authenticate themselves by
-            calling their doctor&#39;s offices. In others doctors have to hand
-            over one-time-passwords on paper.
+            Therefore, we are suggesting a new system that allows users to “prove” their skills, which we believe that will also help recruiters save time in the interview process.
           </p>
+            <h3>Badges System</h3>
           <p>
-            All solutions have in common that as a doctor you have to open a web
-            app to send a file. In most apps you have to log in every time. Next
-            the patient has to log in to the patient portal to retrieve the
-            file.
+            This system allows user to upgrade their badges by earning points by certain ways.
           </p>
+            <h4>Work Experiences / Projects</h4>
           <p>
-            The biggest flaw: The company that operates the system has a
-            complete list of all patients, including their insurance and
-            insurance numbers, and all health data flows through the
-            company&#39;s server. And another problem: if the platform provider
-            decides to discontinue the service, all data will disappear
-            instantly.
+            Users can add their experience and projects they worked on. They can also add a video of themselves to talk about those projects and experiences.
           </p>
+            <h4>Certificates / Degrees</h4>
           <p>
-            LifeTime follows another path. It is decentral and mobile first.
-            Encryption keys are stored on the doctor&#39;s computer and medical
-            data is stored on the patient&#39;s smartphone and can from there be
-            pushed to cloud storage or sent to other doctors at the
-            patient&#39;s will.
+            Users can submit their proof of certificates/degrees to earn points. 
+            The points can vary based on the level of the degree. We will look into their courses and GPA to make the decision.
           </p>
+          <p> Ex: Bachelor’s 100 pts. Master’s 150 pts.  </p>
+            <h4>Referrals</h4>
           <p>
-            Doctors can send files to a patient&#39;s smartphone via an
-            end-to-end encrypted connection without the patient having to set up
-            a user account in a web app beforehand. The patient does not even
-            need to have heard of LifeTime before.
+            Users can request other users to approve their skills. 
+            The points for each request can be different based on the level of the ones who make the approval. 
           </p>
+            <h4>Tests / Lectures</h4>
           <p>
-            The assignment between doctor and patient is created using the
-            insurance number and the mobile phone number. Normally, doctors&#39;
-            offices already have both numbers stored in their systems.
+            There are certain tests for skills/experiences they listed in the profile. 
+            Points and requirements can vary depending on the levels.
+            They can also learn from provided lectures to earn points. 
+            The material can be from beginner to advanced topics. 
           </p>
-          <p>
-            With LifeTime the patient is in full possession of his data and the
-            only one being able to decrypt it. The app runs natively and if it
-            is being discontinued in the future, the user has enough time to
-            export the data to another system.
-          </p>
+        <FullSizeImage src={UI2Src} />
+          <h3>Application Progress Bar</h3>
+            <h4><b>For Recruiters</b></h4>
+              <h4>Transparent Recruiting</h4>
+                <p>Share and adhere to a timelineSet probation goalsKey takeaways</p>
+              <h4>Smooth Recruiting</h4>
+                <p>Streamline with technologyBrief candidates before the interview</p>
+            <h4><b>For Candidates</b></h4>
+              <h4>Smart Applying</h4>
+                <p>Track the application statusSave the time to apply for</p>
+              <h4>Motivated Applying</h4>
+                 <p>Prove  your growthInform users of how close they are to completing a set of tasks by showing steps of completion along the way.</p>
         </ContentWrapper>
         <FullSizeImage src={TasksSrc} />
         <ContentWrapper>
-          <h2>The components of LifeTime</h2>
-          <h3>LifeTime for Patients: The Android and iOS Apps</h3>
+          <h2>To make Live Q&A Chat Effectively</h2>
+          <h3>Virtual Job Expo</h3>
           <p>
-            On the patient side, LifeTime provides an Android and an iOS app
-            that serve as electronic medical records and have elaborated
-            features for managing, scanning, receiving and sending files. It is
-            also equipped with sophisticated medication reminder features.
+            Candidate submit questions,
+            Collect questions about the position/company 
           </p>
           <p>
-            Originally, I had designed two relatively different apps for both
-            platforms to meet the platform-specific requirements. Gradually, I
-            brought the apps closer together as the general design differences
-            between Android and iOS became smaller in time. A fact that also
-            saves a lot of design effort and enhances the branding efforts with
-            a uniform look and feel.
+            Analyze similar keyword from questions,
+            Group the topic of the questions using AI,
+            Remove  duplicated questions,
+            Simplify the questions and show on the list
           </p>
           <p>
-            Creating trust in the product has been the biggest challenge for
-            LifeTime from the very beginning. That&#39;s why we gradually
-            eliminated patterns that people were not already familar with from
-            other apps. Custom iconography is only used to add delight, not in
-            places where the meaning of icons and symobls is critical.
-          </p>
-
-          <p>
-            People are already using apps with similar features every day (e. g.
-            chat apps, scan apps or cloud storage apps). We noticed over time
-            that the LifeTime app is more likely to be accepted when familiar
-            patterns are predominantly used and new patterns are only taught in
-            bite-sized form.
-          </p>
-        </ContentWrapper>
-        <FullSizeImage src={UI2Src} />
-        <ContentWrapper>
-          <h3>LifeTime for Doctors: The best interface is no interface</h3>
-          <p>
-            On the doctor&#39;s side LifeTime provides an Electron app for
-            asynchronous, encrypted transmission of files to patients or, as of
-            late, also directly to colleagues.
-          </p>
-          <p>
-            Here I had fewer comparable applications and fewer known patterns to
-            start from. Doctors and medical teams already use many different
-            applications in their daily work, all with their own interfaces:
-            appointment management apps, practice information systems, image
-            viewers, etc. These programs are often large legacy systems with
-            unintuitive design.
-          </p>
-          <p>
-            In the beginning, I created an elaborated interface for sending
-            reports and documents to patients. In the next step I began to
-            eliminate and hide uncritical parts of the UI in order to only show
-            elements really necessary for the task at hand. At the end there was
-            nothing left. The LifeTime Desktop Interface became mostly invisible
-            and only appeared when needed.
-          </p>
-        </ContentWrapper>
-        <EmbedContainer>
-          <Video
-            width="850"
-            height="478"
-            src="https://www.youtube.com/embed/YMUITHc8Xkk?rel=0&amp;showinfo=0"
-            frameBorder="0"
-            allowFullScreen
-          />
-        </EmbedContainer>
-        <ContentWrapper>
-          <p>
-            Doctors only need to log in once. From this moment on, they just
-            have to click on <code>print</code> or alternatively put a file in a
-            designated folder on their desktop.
-          </p>
-          <p>
-            The minimalistic dispatch window pops up and stays in the foreground
-            of the screen to avoid getting lost when the user copies a mobile
-            phone number from the practice management system into the window. As
-            soon as the user clicks on send, the interface disappears again.
-          </p>
-          <p>
-            The challenge in this case was not the visual design of the buttons,
-            but the brainstorming and mockup process to achieve an optimal
-            experience for the practice team.
-          </p>
-          <p>
-            Over time a more visible interface was added back carefully as new
-            features like an inbox were introduced. There too the design is
-            based on known chat apps to have the least onboarding friction
-            possible.
-          </p>
-        </ContentWrapper>
-        <FullSizeImage src={DesktopSrc} />
-        <ContentWrapper>
-          <h2>Outlook</h2>
-          <p>
-            We live in exciting times for digital health. Apps are maturing
-            slowly and the acceptance of tools that really help doctors and
-            patients in their daily communication and interaction is increasing
-            from day to day.
-          </p>
-          <p>
-            Digital health has long been synonymous with fitness and nutrition
-            apps or quantified self tools.
-          </p>
-          <p>
-            I believe that the real potential will only unfold when software not
-            only helps young, healthy people to stay healthy, but actually
-            supports chronically sick, elderly people or others really in need
-            of care, when it helps doctors in their daily work and creates
-            transparency for all sides.
+            Candidates vote for the questions that they want to ask,
+            View the answers with the search bar,
+            Recruiters answer the general question list 
           </p>
         </ContentWrapper>
       </Section>
