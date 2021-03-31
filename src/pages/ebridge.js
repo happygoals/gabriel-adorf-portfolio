340540dd597dc5726370b458a93768f93f80a207
Haemin Ryu <13105504+happygoals@users.noreply.github.com>
2021-03-31 11:07:48 -0400

    Update ebridge.js

--- src/pages/ebridge.js
+++ src/pages/ebridge.js
@@ -106,10 +106,10 @@ const Ebridge = () => {
       <CoverImage src={CoverSrc} focusX={'35%'} focusY={'50%'} />
       <Section>
         <ContentWrapper>
-          <h2>Covid 19 has caused the disruption in face-to-face interaction between recruiters and candidates.</h2>
+          <h2>Covid 19 has disrupted face-to-face interaction between recruiters and candidates.</h2>
           <h3>Shallow networking and resume spreading does not help with recruiting.</h3>
           <p>
-            Students want to interact with professionals who understand the position. There are not many virtual tool that gives candidates more chances to get reached and hired. 
+            Students want to interact with professionals who understand the position. There are not many virtual tools that give candidates more chances to get reached and hired. 
           </p>
             <FullSizeImage src={ProblemSrc} />
           
@@ -117,7 +117,7 @@ const Ebridge = () => {
          
           <p>
             Resumes are a great way by which you can identify quality talent but they have a major problem: recruiters have a hard time managing and organizing these resumes to pick the relevant ones. 
-            E-bridge is the tools to collect, parse, search, and manage resumes from your job seekers. The features of E-bridge extend beyond just managing resumes and sorting them into stacks, but into the customized hiring process.
+            E-bridge is the tool to collect, parse, search, and manage resumes from your job seekers. The features of E-bridge extend beyond just managing resumes and sorting them into stacks, but into the customized hiring process.
           </p>
     
         </ContentWrapper>
@@ -126,61 +126,59 @@ const Ebridge = () => {
           <h3>Our Activity Diagram</h3>
           <p>
            Each view starts with candidates create profiles and Professionals create their accounts. 
-           The steps candidates would do from their view (Candidate View) and the steps recruiters would do from their view (Professional View). 
+           The steps candidates would do from their view (Candidate View), and the steps recruiters would do from their view (Professional View). 
           </p>
         </ContentWrapper>
         <ContentWrapper>
           <h2> Project Goals</h2> 
           <h3> Instant Connect </h3>
-            <p> It allow users to connect instantly to dream candidates and jobs. </p>
+            <p> It allows users to connect instantly to dream candidates and jobs. </p>
           <h3> Right Candidates </h3>
-            <p> Recruiters can get to know candidates beyond the resume line.</p>
+            <p> Recruiters can get to know candidates beyond the resume line. </p>
           <h3> Self-improvement </h3>
-            <p> User can understande most valuable skill, train and get certified, get challenged through projects.</p>
+            <p> Users can understand the most valuable skill, train and get certified, get challenged through projects. </p>
           <h2>Our approach</h2>     
          </ContentWrapper>
           <FullSizeImage src={ResumeGallery} />
         <ContentWrapper>
           <h3>Resume Gallery & Video Gallery</h3>
           <p>
-            Candidates must upload their resume on the system as they create their profile.
-            After finishing the profile, our system will have predefined suggested questions for them to address in a short 3 minutes video. They must complete this step for the profile to be visible to recruiters.
-            After they completed these steps, besides demonstrated in their home page, the resume and video will be added to the gallery, which is organized by majors. 
+            Candidates must upload their resumes on the system as they create their profiles. 
+            After finishing the profile settings, our system will have predefined suggested questions for the candidates to address in a short 3 minutes video. 
+            They must complete these steps for the profile to be visible to recruiters on the gallery. 
+            E-bridge adds the candidate's resumes and videos to the resume & video gallery. 
+            The galleries are categorized by a particular group, such as job title, major, and location.
 
           </p>
           <h3>Resume Gallery</h3>
           <p>
-            Visible to recruiters by school / majors / work authorization, etc.
-            This feature can be used as a resume cloud for the recruiter.
-            The system suggests resume to include the predefined keywords once candidate upload their resume. Recruiters can search by skills and find the resume containing the skills they need.
-            Wish-list / favorite resume increase points for candidate.
-            Algorithm to suggest potential resumes to recruiters based on the current openings will help the initial recruiting process management. 
+            Visible to recruiters by school/majors/work authorization, etc. This feature can be used as a resume cloud for the recruiter. The system suggests resume to include the predefined keywords once candidate uploads their resume. Recruiters can search by skills and find the resumes containing the skills they need. Wish-list / favorite resume increase points for the candidate. An algorithm to suggest potential resumes to recruiters based on the current openings will help the initial recruiting process.
           </p>
           <h3>Video Gallery</h3>
           <p>
-            Videos will be visible to recruiters by college majors / positions.
-            The video is hoped to eliminate the need for phone screen interview and help recruiter select those that will move straight to the interview with people in the position.
-            Recruiter can use wish-list option for keeping their favorite videos and they can refer once they have an opening position.
-            Algorithm to suggest videos to recruiters based on the current openings  will help the interview process management. 
+            Videos will be visible to recruiters by college majors/positions.
+            This video gallery feature eliminates the need for phone screen interviews and helps recruiters select people to go straight to interviews with people in their positions.
+            Recruiters can use the wish list option to archive their favorite videos and can promote candidates once the recruiter takes the opening position.
+            An algorithm to suggest videos to recruiters based on the current openings will help the interview process management. 
           </p>
           
         </ContentWrapper>
         <ContentWrapper>
           <h3>Earning Badges</h3>
           <p>
-            Current job-seeking platforms allow users to add skills to their profiles. 
-            However, it is a fact that it is difficult to know if those listed skills are <b>VALID</b>.
+            Current job-seeking platforms allow users to add skills to their profiles. However, it is a fact that it is difficult to know if those listed skills are <b>VALID</b>.
           </p>
           <p>
-            Therefore, we are suggesting a new system that allows users to “<b>prove</b>” their skills, which we believe that will also help recruiters save time in the interview process.
+            Therefore, a new system, E-bridge Badges System, will allow candidates to “<b>prove</b>” their skills, which will also help recruiters save time in the interview process.
           </p>
             <h3>Badges System</h3>
           <p>
-            This system allows user to upgrade their badges by earning points by certain ways.
+            This system allows user to upgrade their badges by earning points in reliable ways.
           </p>
             <h4>Work Experiences / Projects</h4>
           <p>
-            Users can add their experience and projects they worked on. They can also add a video of themselves to talk about those projects and experiences.
+            Users can add their experience and projects they have completed. 
+            They can also add a video of themselves to talk about those projects and experiences.
           </p>
             <h4>Certificates / Degrees</h4>
           <p>
@@ -191,26 +189,30 @@ const Ebridge = () => {
             <h4>Referrals</h4>
           <p>
             Users can request other users to approve their skills. 
-            The points for each request can be different based on the level of the ones who make the approval. 
+            The points for each request can be different based on the job level of who makes the approval.
           </p>
             <h4>Tests / Lectures</h4>
           <p>
-            There are certain tests for skills/experiences they listed in the profile. 
-            Points and requirements can vary depending on the levels.
-            They can also learn from provided lectures to earn points. 
-            The material can be from beginner to advanced topics. 
+            There are tests/exams for skills/experiences listed in the profile. 
+            Points and requirements can vary depending on the levels. 
+            Users can earn points from the lectures that are guaranteed. The material of the courses can be from beginner to advanced topics.
           </p>
           <h3>Application Progress Bar</h3>
             <h4><b>For Recruiters</b></h4>
               <h4>Transparent Recruiting</h4>
-                <p>Share and adhere to a timelineSet probation goalsKey takeaways</p>
+                <p>Share and adhere to a timeline</p>
+                <p>Set probation goals</p>
+                <p>Key takeaways</p>
               <h4>Smooth Recruiting</h4>
-                <p>Streamline with technologyBrief candidates before the interview</p>
+                <p>Streamline with technology</p>
+                <p>Brief candidates before the interview</p>
             <h4><b>For Candidates</b></h4>
               <h4>Smart Applying</h4>
-                <p>Track the application statusSave the time to apply for</p>
+                <p>Track the application status</p>
+                <p?Save the time to apply for</p>
               <h4>Motivated Applying</h4>
-                 <p>Prove  your growthInform users of how close they are to completing a set of tasks by showing steps of completion along the way.</p>
+                 <p>Prove  your growth</p>
+                 <p>Inform users of how close they are to completing a series of tasks by showing the completion steps along the way.</p>
         </ContentWrapper>
         <ContentWrapper>
           <h2>To make Live Q&A Chat Effectively</h2>
@@ -223,7 +225,7 @@ const Ebridge = () => {
             Analyze similar keyword from questions,
             Group the topic of the questions using AI,
             Remove  duplicated questions,
-            Simplify the questions and show on the list
+            Simplify the questions and show them on the list
           </p>
           <p>
             Candidates vote for the questions that they want to ask,
